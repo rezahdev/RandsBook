6a6ac6dabbde3cfe1915b0a2b5532b5351dbaf70
Reza Saker <reza.shossain@gmail.com>
2022-08-07 08:44:37 -0500

    Started the project and finished login and registration features

---
+++ README.md
@@ -0,0 +1,3 @@
+#RandsBook
+
+##Under Development
---
+++ resources/views/components/auth-card.blade.php
@@ -0,0 +1,13 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div>
+        <h1 id="auth_page_logo">RandsBook</h1>
+    </div>
+
+    <div class="w-11/12 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+
+    <div class="text-center pt-16">
+        <p>All rights reserved &copy; RandsBook.com</p>
+    </div>
+</div>
