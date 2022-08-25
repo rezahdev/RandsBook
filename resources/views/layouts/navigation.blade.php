d73370aaa3395da8b41ab269af29b5e15126e725
Reza Saker <saker.reza001@hotmail.com>
2022-08-25 01:26:00 -0500

    Fixed a bug

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-m font-medium text-black border rounded border-gray-300 py-1 px-3 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <button class="flex items-center text-m font-small text-black border rounded border-gray-300 py-1 px-3 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -47,6 +47,10 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <x-dropdown-link :href="route('books.create')">
+                                {{ __('Profile') }}
+                        </x-dropdown-link>
+                        
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
@@ -57,9 +61,6 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
-                        <x-dropdown-link :href="route('books.create')">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -97,7 +98,7 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-small text-base text-black">Profile</div>
             </div>
 
             <div class="mt-3 space-y-1">
