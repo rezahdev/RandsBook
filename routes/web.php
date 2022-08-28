3a073a9fe16eee3a21400e344fbce9f7de022059
Reza Saker <saker.reza001@hotmail.com>
2022-08-28 06:32:25 -0500

    Formatted route file

--- routes/web.php
+++ routes/web.php
@@ -22,25 +22,26 @@ Route::get('/search/{edition_key}', [BookController::class, 'show_from_search_re
 Route::get('/add', [BookController::class, 'create'])->middleware('auth')->name('books.create');
 Route::get('/add/{edition_key}', [BookController::class, 'create_with_data'])->middleware('auth')->name('books.create_with_data');
 Route::get('/book/{id}/edit', [BookController::class, 'edit'])->middleware('auth')->name('books.edit');
+Route::get('/wishlist', [BookController::class, 'wishlist'])->middleware('auth')->name('books.wishlist');
+Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile.index');
+Route::get('/profile/changePassword', [ProfileController::class, 'change_password'])->middleware('auth')->name('profile.change_password');
+Route::get('/credits', function() { return view('credits'); });
+
+
 Route::post('/store', [BookController::class, 'store'])->middleware('auth')->name('books.store');
+Route::post('/wishlist/add', [BookController::class, 'add_to_wishlist'])->middleware('auth')->name('books.add_to_wishlist');
+
 
 Route::put('/update', [BookController::class, 'update'])->middleware('auth')->name('books.update');
-Route::delete('/delete/{id}', [BookController::class, 'delete'])->middleware('auth')->name('books.delete');
-
 Route::put('/updateReadPages', [BookController::class, 'update_read_pages'])->middleware('auth')->name('books.update_read_pages');
-
-Route::get('/wishlist', [BookController::class, 'wishlist'])->middleware('auth')->name('books.wishlist');
-Route::post('/wishlist/add', [BookController::class, 'add_to_wishlist'])->middleware('auth')->name('books.add_to_wishlist');
-Route::delete('/wishlist/remove', [BookController::class, 'remove_from_wishlist'])->middleware('auth')->name('books.remove_from_wishlist');
-Route::put('/wishlistToLibrary', [BookController::class, 'wishlist_to_library'])->middleware('auth')->name('books.wishlist_to_library');
-Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile.index');
-
 Route::put('/profile/updateName', [ProfileController::class, 'update_name'])->middleware('auth')->name('profile.update_name');
 Route::put('/profile/updateNickname', [ProfileController::class, 'update_nickname'])->middleware('auth')->name('profile.update_nickname');
 Route::put('/profile/updateEmail', [ProfileController::class, 'update_email'])->middleware('auth')->name('profile.update_email');
-Route::get('/profile/changePassword', [ProfileController::class, 'change_password'])->middleware('auth')->name('profile.change_password');
 Route::put('/profile/updatePassword', [ProfileController::class, 'update_password'])->middleware('auth')->name('profile.update_password');
-Route::get('/credits', function() {
-    return view('credits');
-});
+Route::put('/wishlistToLibrary', [BookController::class, 'wishlist_to_library'])->middleware('auth')->name('books.wishlist_to_library');
+
+
+Route::delete('/delete/{id}', [BookController::class, 'delete'])->middleware('auth')->name('books.delete');
+Route::delete('/wishlist/remove', [BookController::class, 'remove_from_wishlist'])->middleware('auth')->name('books.remove_from_wishlist');
+
 require __DIR__.'/auth.php';
